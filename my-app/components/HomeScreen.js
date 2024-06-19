ef1424b2c5bf852ea1f41c0ad368566a704c25a9
11328476-DCIT205 <paakofi.one@gmail.com>
2024-06-19 12:32:51 +0000

    Additions to home screen

--- my-app/components/HomeScreen.js
+++ my-app/components/HomeScreen.js
@@ -1,28 +1,66 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { Text,View, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet, ScrollView, TextInput } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
-export default function HomeScreen(){
-    
-    const HomeScreen = ({ route }) => {
-      const { name, email } = route.params;
-    
-      return (
-        <View style={styles.container}>
-          <Text style={styles.text}>Name: {name}</Text>
-          <Text style={styles.text}>Email: {email}</Text>
+export default function HomeScreen({ route }) {
+  const { name, email } = route.params;
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <View style={styles.profileContainer}>
+        <View style={styles.profileInfo}>
+          <View>
+            <Text style={styles.profileName}>{name}</Text>
+            <Text style={styles.profileEmail}>{email}</Text>
+          </View>
+          <Icon name='account-circle' size={50} style={styles.profileIcon} />
         </View>
-      );
-    };
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      text: {
-        fontSize: 18,
-        marginVertical: 10,
-      },
-    });
+      </View>
+      <View style={styles.search}>
+        <Icon name="magnify" size={24}/>
+        <TextInput
+        placeholder='Search a job or position'
+        />
+      </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  profileContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    marginBottom: 16,
+  },
+  profileInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  profileIcon: {
+    marginRight: 16,
+  },
+  profileName: {
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  profileEmail: {
+    color: 'gray',
+    fontSize: 14,
+  },
+  search:{
+    backgroundColor: '#f2f2f2',
+    width: 250,
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    marginTop: 16,
+    borderRadius: 8 
+  }
+});
